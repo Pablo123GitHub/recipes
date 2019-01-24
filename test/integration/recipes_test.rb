435df3877a892883178c36ee41292f08a6ca9633
Pablo <paul.ladiv123@gmail.com>
2019-01-24 12:13:37 +0000

    add return button on the show page

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -30,6 +30,7 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @chef.chefname, response.body 
     assert_select "a[href=?]", edit_recipe_path(@recipe), text: "Edit recipe"
     assert_select "a[href=?]", recipe_path(@recipe), text: "Delete recipe"
+    assert_select "a[href=?]", recipes_path, text: "Go back to Recipes list"
   end 
 
  test "create new valid recipe" do   
